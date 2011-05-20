9d48b32e917e082aee586da5fafce073beef7004
Allan Lei <allanlei@helveticode.com>
2011-05-20 11:07:27 -0700

    Add username generate

--- rhinocloud/utils/__init__.py
+++ rhinocloud/utils/__init__.py
@@ -1,4 +1,7 @@
 from django.core.validators import validate_email
+import random
+import string
+
 
 def extract_domain(email):
     try:
@@ -8,3 +11,7 @@ def extract_domain(email):
         return None
 
 get_email_domain = extract_domain
+
+def random_generator(base_string, max_length=30, random_string=(string.ascii_uppercase + string.digits)):
+    base = base_string[:max_length]
+    return '%s%s' % (base, ''.join([random.choice(random_string) for i in range(max_length - len(base))]))
