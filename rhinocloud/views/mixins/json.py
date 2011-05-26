e42f50676a498a8cad62bc19e305b69cd1ee4c6f
Allan Lei <allanlei@helveticode.com>
2011-05-26 10:45:42 -0700

    Rename Add serialize options remove ajax check

--- rhinocloud/views/mixins/json.py
+++ rhinocloud/views/mixins/json.py
@@ -4,9 +4,7 @@ from django.utils import simplejson as json
 
 class JSONResponseMixin(object):
     def render_to_response(self, context, **kwargs):
-        if self.is_json_request():
-            return self.get_json_response(self.convert_context_to_json(context))
-        return super(JSONResponseMixin, self).render_to_response(context, **kwargs)
+        return self.get_json_response(self.convert_context_to_json(context))
 
     def get_json_response(self, content, **httpresponse_kwargs):
         return HttpResponse(content, content_type='application/json', **httpresponse_kwargs)
@@ -18,19 +16,42 @@ class JSONResponseMixin(object):
         return self.request.is_ajax() and 'application/json' in self.request.META['HTTP_ACCEPT']
 
 
-class QuerysetJSONResponseMixin(JSONResponseMixin):
-    serialize_fields = ()
-    serialize_relations = ()
-    serialize_extras = ()
+class JSONQuerysetResponseMixin(object):
+    serialize_fields = None
+    serialize_relations = None
+    serialize_extras = None
+    serialize_excludes = None
     
     def get_serialize_fields(self):
-        return self.serialize_fields
+        if self.serialize_fields:
+            fields = self.serialize_fields
+        else:
+            fields = ()
+#            raise ImproperlyConfigured('Provide serialize_fields or override get_serialize_fields().')
+        return fields
     
     def get_serialize_relations(self):
-        return self.serialize_relations
+        if self.serialize_relations:
+            relations = self.serialize_relations
+        else:
+            relations = ()
+#            raise ImproperlyConfigured('Provide serialize_relations or override get_serialize_relations().')
+        return relations
     
     def get_serialize_extras(self):
-        return self.serialize_extras
+        if self.serialize_extras:
+            extras = self.serialize_extras
+        else:
+            extras = ()
+#            raise ImproperlyConfigured('Provide serialize_extras or override get_serialize_extras().')
+        return extras
+    
+    def get_serialize_excludes(self):
+        if self.serialize_excludes:
+            fields = self.serialize_excludes
+        else:
+            fields = ()
+        return fields
         
     def render_to_response(self, context, serializer_kwargs={}, **kwargs):
         return self.get_json_response(self.convert_to_json(self.get_queryset(), **serializer_kwargs), **kwargs)
@@ -38,10 +59,11 @@ class QuerysetJSONResponseMixin(JSONResponseMixin):
     def get_json_response(self, content, **httpresponse_kwargs):
         return HttpResponse(content, content_type='application/json', **httpresponse_kwargs)
     
-    def convert_to_json(self, queryset, **kwargs):
+    def convert_to_json(self, queryset, **kwargs):  
         kwargs.update({
             'relations': self.get_serialize_relations(),
             'extras': self.get_serialize_extras(),
             'fields': self.get_serialize_fields(),
+            'excludes': self.get_serialize_excludes(),
         })
         return serializers.serialize('json', queryset, **kwargs)
