2edb2145f6f7447a7c659d7eeb51c7b75aa0c6d4
Allan Lei <allanlei@helveticode.com>
2011-05-30 12:56:13 -0700

    Add generate username signal

--- rhinocloud/contrib/auth/signals.py
+++ rhinocloud/contrib/auth/signals.py
@@ -2,6 +2,13 @@ from django.contrib.auth.models import User
 from rhinocloud.utils import random_generator
 
 
+def generate_username_from_email(sender, instance, **kwargs):
+    if sender == User:
+        username = instance.email
+        if len(username) > 30:
+            username = random_generator(username[:25])
+        instance.username = username
+            
 def username_shorten(sender, instance, **kwargs):
     if sender == User:
         if len(instance.username) > 30:
