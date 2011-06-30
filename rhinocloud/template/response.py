d43e5c3686afa2d15d6287758bc1fea5572fb89e
Allan Lei <allanlei@helveticode.com>
2011-06-30 13:31:52 -0700

    Add Webkit PDF generator

--- rhinocloud/template/response.py
+++ rhinocloud/template/response.py
@@ -4,6 +4,8 @@ from django.template import loader
 from rhinocloud.utils.pdf import convert_to_pdf
 from rhinocloud.template import openoffice
 
+from subprocess import Popen, PIPE
+
 class PDFTemplateResponse(TemplateResponse):
     def __init__(self, pdf_kwargs={}, *args, **kwargs):
         super(PDFTemplateResponse, self).__init__(*args, **kwargs)
@@ -15,5 +17,19 @@ class PDFTemplateResponse(TemplateResponse):
         return convert_to_pdf(rendered_template, **self.pdf_kwargs)
 
 
+class WebkitPDFTemplateResponse(TemplateResponse):
+    def __init__(self, pdf_kwargs={}, *args, **kwargs):
+        super(WebkitPDFTemplateResponse, self).__init__(*args, **kwargs)
+        self.pdf_kwargs = pdf_kwargs
+    
+    def get_pdf_kwargs_string(self):
+        return ' '.join([' '.join('--%s %s'.split(' ')) % (key, value) for key, value in self.pdf_kwargs.items()])
+        
+    @property
+    def rendered_content(self):
+        rendered_template = super(WebkitPDFTemplateResponse, self).rendered_content
+        p = Popen('wkhtmltopdf - - %s' % self.get_pdf_kwargs_string(), shell=True, stdin=PIPE, stdout=PIPE)
+        return p.communicate(rendered_template)[0]
+        
 class OpenOfficeTemplateResponse(TemplateResponse):
     pass
--- rhinocloud/views/generic/pdf.py
+++ rhinocloud/views/generic/pdf.py
@@ -1,9 +1,13 @@
 from django.views import generic
-from rhinocloud.template.response import PDFTemplateResponse
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
+from django.template import loader
+from django.core.servers.basehttp import FileWrapper
+
+from rhinocloud.template.response import PDFTemplateResponse, WebkitPDFTemplateResponse
 
 
-
-class PDFView(generic.base.TemplateView):
+class PDFResponseMixin(object):
     response_class = PDFTemplateResponse
     
     def get_pdf_kwargs(self):
@@ -13,6 +17,51 @@ class PDFView(generic.base.TemplateView):
         
     def render_to_response(self, context, content_type='application/pdf', **kwargs):
         return super(PDFView, self).render_to_response(
+            context, 
+            content_type=content_type, 
+            pdf_kwargs=self.get_pdf_kwargs(), 
+            **kwargs)
+
+class PDFView(PDFResponseMixin, generic.base.TemplateView):
+    pass
+    
+    
+class WebkitPDFResponseMixin(object):
+    response_class = WebkitPDFTemplateResponse
+    pdf_css_file = None
+    pdf_title = None
+    
+    def get_pdf_css_file(self):
+        return self.pdf_css_file
+
+    def get_pdf_title(self):
+        return self.pdf_title
+        
+    def get_pdf_kwargs(self):
+        kwargs = {
+            'margin-top': '0mm',
+            'margin-bottom': '0mm',
+            'margin-left': '0mm',
+            'margin-right': '0mm',
+            'encoding': 'UTF-8',
+            'ignore-load-errors': '',
+            'minimum-font-size': '5',
+            'quiet': '',
+            'orientation': 'Portrait',
+        }
+        if self.get_pdf_css_file():
+            kwargs.update({
+                'user-style-sheet': self.get_pdf_css_file(),
+            })
+            
+        if self.get_pdf_title():
+            kwargs.update({
+                'title': self.get_pdf_title(),
+            })
+        return kwargs
+        
+    def render_to_response(self, context, content_type='application/pdf', **kwargs):
+        return super(WebkitPDFResponseMixin, self).render_to_response(
             context, 
             content_type=content_type, 
             pdf_kwargs=self.get_pdf_kwargs(), 
