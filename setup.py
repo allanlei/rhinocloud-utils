6c7327d50fe625358f4af0a88e1af05e7f99a788
Allan Lei <allanlei@helveticode.com>
2011-08-26 15:55:35 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def find_packages_in(where, **kwargs):
 
 setup(
     name = 'rhinocloud-utils',
-    version = '0.2.1',
+    version = '0.2.2',
     author = 'Allan Lei',
     author_email = 'allanlei@helveticode.com',
     description = ('Django utility functions'),
